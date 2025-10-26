ea0f694b51942a4dbf251ddf0cf681e41f9fca0c
maxle5 <max.lefebvre@garvin-allen.com>
2025-10-26 10:55:09 -0300

    entrypoint script created

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail  # safer bash options
+shopt -s nullglob  # makes *.sh expand to nothing if no match (instead of literal string)
+
+MIGRATIONS_DIR="./migrations"
+STATE_DIR="$HOME/.local/state/maxos/migrations"
+SCRIPTS=("$MIGRATIONS_DIR"/*.sh)
+
+echo "------------------------------------------"
+echo "Running migrations"
+echo "------------------------------------------"
+
+# Loop through each script and execute it
+for script in "${SCRIPTS[@]}"; do
+  script_name=$(basename "$script")
+  state_file="$STATE_DIR/$script_name"
+
+  if [[ -e "$state_file" ]]; then
+    echo "⏩ Skipping $script_name as it was already applied"
+    continue
+  fi
+
+  bash "$script"
+  echo "✅ Applied $script_name"
+
+  # mark the script as applied
+  touch "$state_file"
+done
+
+echo "All migrations executed successfully!"
