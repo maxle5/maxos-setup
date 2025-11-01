5fd091bef33df2f168d7b97b877c399c9ceced07
maxle5 <max.lefebvre@garvin-allen.com>
2025-11-01 09:21:43 -0300

    removed dupe pkg

--- migrations/1761490802.sh
+++ migrations/1761490802.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
 # "core" apps
-yay -S btrfs-assistant-git \
-          dbeaver-ce-jre-bin \
-          docker \
-          docker-buildx \
-          dolphin \
-          gimp \
-          google-chrome \ 
-          neovim \
-          network-manager-applet \
-          nosqlbooster-mongodb \
-          p4v \
-          pavucontrol \
-          remmina \
-          rider \
-          slack-desktop \
-          sourcegit-bin \
-          spotify \
-          visual-studio-code-bin \
-          wezterm-git \
-          wofi \
-          wofi-emoji \
-       --noconfirm
+yay -S dbeaver-ce-jre-bin \
+       docker \
+       docker-buildx \
+       dolphin \
+       gimp \
+       google-chrome \ 
+       neovim \
+       network-manager-applet \
+       nosqlbooster-mongodb \
+       p4v \
+       pavucontrol \
+       remmina \
+       rider \
+       slack-desktop \
+       sourcegit-bin \
+       spotify \
+       visual-studio-code-bin \
+       wezterm-git \
+       wofi \
+       wofi-emoji \
+    --noconfirm
 
