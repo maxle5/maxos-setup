ac372419ea310c56a00048b894f2828c7c2c9470
maxle5 <max.lefebvre@garvin-allen.com>
2025-11-01 10:12:15 -0300

    fixed stow

--- run.sh
+++ run.sh
@@ -7,33 +7,28 @@ MIGRATIONS_DIR="./migrations"
 STATE_DIR="$HOME/.local/state/maxos/migrations"
 SCRIPTS=("$MIGRATIONS_DIR"/*.sh)
 
-echo "------------------------------------------"
-echo "Installing Updates"
-echo "------------------------------------------"
+echo "⏳Installing Updates"
 sudo sudo pacman -Syu --noconfirm
 
-echo "------------------------------------------"
-echo "Running migrations"
-echo "------------------------------------------"
-
+echo "⏳Running Migrations"
 # Loop through each script and execute it
 for script in "${SCRIPTS[@]}"; do
   script_name=$(basename "$script")
-  state_file="$STATE_DIR/$script_name"
+  state_file="$STATE_DIR/${script_name%.*}"
 
   if [[ -e "$state_file" ]]; then
     echo "⏩ Skipping $script_name as it was already applied"
     continue
   fi
 
+  echo "⏳Applying $script_name"
   bash "$script"
-  echo "✅ Applied $script_name"
 
   # mark the script as applied
   touch "$state_file"
 done
 
-stow -v -d ./dotfiles -t ~ */
-echo "✅ Updated dotfiles"
+echo "⏳Updating dotfiles"
+stow -d dotfiles -S $(exa -d dotfiles/*/ | xargs -n1 basename) -t ~/
 
-echo "All migrations executed successfully! Please reboot your system."
+echo "✅ Your system has been setup successfully! Please reboot."
